a9a5f2cc38440536a8c9a4a582a6890c167976cd
KidDipper <jyokaeki0208@gmail.com>
2025-08-31 22:48:33 +0900

    add comment

--- src/formattest.c
+++ src/formattest.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-int*add ( int a,int b ){int result=a+b;   return &result; }
+int* add(int a, int b) {
+  int result = a + b;
+  return &result;
+}
 
-int main(){
-int x=1;int y=2;
- int* z=add(x,y);printf("sum=%d\n",*z);
-return 0;}
+int main() {
+  int x = 1;
+  int y = 2;
+  int* z = add(x, y);
+  printf("sum=%d\n", *z);
+  return 0;
+}
+
+// clang-format -i src/util.cでファイルを整形、もしくはファイルで右クリックからformat documentを実行
